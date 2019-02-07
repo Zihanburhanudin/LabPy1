e639662dd08a36af4b45bbb4eebce6750ec9e5c0
Zihanburhanudin <zihan.arj@gmail.com>
2019-02-07 20:45:42 +0700

    Hasil Screenshot

---
+++ program.py
@@ -0,0 +1,24 @@
+def pengulangan():
+    a=int(input('Bilangan Pertama : '))
+    b=int(input('Bilangan Kedua   : '))
+    c=int(input('Bilangan Ketiga  : '))
+
+    if a>b:
+        if a>c:
+            print ('Bilangan terbesarnya adalah :',a)
+        else:
+            print ('Bilangan terbesarnya adalah :',c)
+    else:
+        if b>c:
+            print ('Bilangan terbesarnya adalah :',b)
+        else:
+            print ('Bilangan terbesarnya adalah :',c)
+
+    print ('')
+    print ('Ingin coba lagi? (Y/T)')
+    x=input()
+    if x=='Y':
+        return pengulangan()
+    if x=='T':
+        print('Terimakasih telah menggunakan program ini.')
+pengulangan ()
